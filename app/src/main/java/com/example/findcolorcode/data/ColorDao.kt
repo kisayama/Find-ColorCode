9bb6843d6c17399fb58ae4e8b3a7edc2a429c434
izumi <izumi@c538.local>
2024-09-28 22:11:08 +0900

    MaterialThemeAdd

---
+++ app/src/main/java/com/example/findcolorcode/data/ColorDao.kt
@@ -0,0 +1,4 @@
+package com.example.findcolorcode.data
+
+interface ColorDao {
+}
--- app/src/main/java/com/example/findcolorcode/model/FavoriteColorDataClass.kt
+++ app/src/main/java/com/example/findcolorcode/model/FavoriteColorDataClass.kt
@@ -1,12 +1,13 @@
 package com.example.findcolorcode
 
+import androidx.room.PrimaryKey
 import com.squareup.moshi.JsonClass
 import java.time.LocalDateTime
 import java.util.UUID
 
 @JsonClass(generateAdapter = true)
 data class FavoriteColorDataClass(
-    val id:String = UUID.randomUUID().toString(),
+    @PrimaryKey val id:String = UUID.randomUUID().toString(),
     val colorCode:String,
     var colorName :String,
     var colorMemo: String,
