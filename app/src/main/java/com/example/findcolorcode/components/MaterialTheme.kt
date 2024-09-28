42103142adfda9e9031f20d137d17c97c0f57983
izumi <izumi@c538.local>
2024-09-28 23:06:57 +0900

    Do Theme after Setting

--- app/src/main/java/com/example/findcolorcode/components/MaterialTheme.kt
+++
@@ -1,4 +0,0 @@
-package com.example.findcolorcode.components
-
-class MaterialTheme {
-}
---
+++ app/src/main/java/com/example/findcolorcode/ui/theme/MaterialTheme.kt
@@ -0,0 +1,17 @@
+package com.example.findcolorcode.components
+
+import android.provider.CalendarContract
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+
+private val LightColorPalette = lightColors(
+
+)
+@Composable
+fun JetnewsTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
+
+}
