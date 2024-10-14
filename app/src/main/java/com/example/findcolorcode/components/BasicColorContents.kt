eba3228b1ef1793f711853caf4cee8d96fde3639
izumi <izumi@c538.local>
2024-10-14 21:49:28 +0900

    サイズの調整: Modifier部分の順序を変更してレイアウトを修正

--- app/src/main/java/com/example/findcolorcode/components/BasicColorContents.kt
+++ app/src/main/java/com/example/findcolorcode/components/BasicColorContents.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,53 +21,58 @@ fun BasicColorContents(colorList1: List<String>,colorList2: List<String>,
                   colorList3: List<String>,onBasicSquareSelected: (String) -> Unit){
  Column (modifier = Modifier
           .fillMaxWidth()
-          ,verticalArrangement = Arrangement.spacedBy(10.dp)//オブジェクトごとに垂直方向に間隔を入れる
+          .padding(top = 16.dp),
+          verticalArrangement = Arrangement.spacedBy(10.dp)//オブジェクトごとに垂直方向に間隔を入れる
   ) {
   //縦に3つ並べる
-  BasicColorRow(colorList = colorList1,onBasicSquareSelected)
-  BasicColorRow(colorList = colorList2,onBasicSquareSelected)
-  BasicColorRow(colorList = colorList3,onBasicSquareSelected)
+  BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList1,onBasicSquareSelected)
+  BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList2,onBasicSquareSelected)
+  BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList3,onBasicSquareSelected)
  }
 }
 
 @Composable
-fun BasicColorRow(colorList: List<String>,onBasicSquareSelected: (String) -> Unit){
- //横にsquare4つ並べる
-  Row (modifier = Modifier
+fun BasicColorRow(
+ modifier: Modifier = Modifier,
+ colorList: List<String>,
+ onBasicSquareSelected: (String) -> Unit
+) {
+ Row(
+  modifier = Modifier
    .fillMaxWidth()
-   .padding(start = 8.dp),
-   horizontalArrangement = Arrangement.Center,
-   ){
-   BasicColorSquare(
-    modifier = Modifier.weight(4f),
-    colorCode =colorList[0],
-    onBasicSquareSelected = onBasicSquareSelected
-   )
-   BasicColorSquare(
-    modifier = Modifier.weight(4f),
-    colorCode =colorList[1],
-    onBasicSquareSelected = onBasicSquareSelected
-   )
-   BasicColorSquare(
-    modifier = Modifier.weight(4f),
-    colorCode =colorList[2],
-    onBasicSquareSelected = onBasicSquareSelected
-   )
-   BasicColorSquare(
-    modifier = Modifier.weight(4f),
-    colorCode =colorList[3],
-    onBasicSquareSelected = onBasicSquareSelected
-   )
-  }
+   .padding(start = 8.dp, end = 8.dp),
+  horizontalArrangement = Arrangement.SpaceEvenly,
+ ) {
+  BasicColorSquare(
+   colorCode = colorList[0],
+   onBasicSquareSelected = onBasicSquareSelected
+  )
+  BasicColorSquare(
+   colorCode = colorList[1],
+   onBasicSquareSelected = onBasicSquareSelected
+  )
+  BasicColorSquare(
+   colorCode = colorList[2],
+   onBasicSquareSelected = onBasicSquareSelected
+  )
+  BasicColorSquare(
+   colorCode = colorList[3],
+   onBasicSquareSelected = onBasicSquareSelected
+  )
+ }
 }
+
+
 
 @Composable
  fun BasicColorSquare(
- modifier: Modifier, colorCode:String,
+ modifier: Modifier = Modifier,
+ colorCode:String,
  onBasicSquareSelected: (String)->Unit){
   Box (
    modifier = modifier
     .padding(end = 8.dp)
+    .size(70.dp)
     .clickable { onBasicSquareSelected(colorCode) }
     .background(Color(android.graphics.Color.parseColor(colorCode)))
     .border(1.dp, Color.LightGray)
