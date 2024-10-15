d09b7fa02cc41d7b17a8629b24c994f3cec4065d
izumi <izumi@c538.local>
2024-10-16 01:08:10 +0900

    追加:The Color APIと通信を行うためのinterfaceを追加しました。

--- app/src/main/java/com/example/findcolorcode/api/TheColorApiService.kt
+++ app/src/main/java/com/example/findcolorcode/api/TheColorApiService.kt
@@ -1,4 +1,15 @@
 package com.example.findcolorcode.api
 
+import com.example.findcolorcode.model.ColorSchemeResponce
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface TheColorApiService {
+    @GET("https://www.thecolorapi.com/scheme?")
+    suspend fun getColorScheme(
+        @Query("hex") colorCodeWithoutHash :String,// #を除いたcolorCode　例: 0047AB
+        @Query("mode") mode :String = "analogic",//スキームを生成するためのモード デフォルトはanalogic　他にもいくつかある
+        @Query("format") format :String = "json",//結果を受け取る形式を指定する。デフォルトはjson 他にhtml,svg
+        @Query("count") count : Int = 5,//取得するスキームカラーの数　デフォルトは5
+        ): ColorSchemeResponce
 }
