bb20074192814bac8ce824e680424c64ea58d9e1
izumi <izumi@c538.local>
2024-10-16 20:58:38 +0900

    追加:戻り値のデータ型を ColorSchemeResponceとしました

--- app/src/main/java/com/example/findcolorcode/api/TheColorApiService.kt
+++ app/src/main/java/com/example/findcolorcode/api/TheColorApiService.kt
@@ -1,6 +1,6 @@
 package com.example.findcolorcode.api
 
-import com.example.findcolorcode.model.ColorSchemeResponce
+import com.example.findcolorcode.model.ColorSchemeResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,5 +11,5 @@ interface TheColorApiService {
         @Query("mode") mode :String = "analogic",//スキームを生成するためのモード デフォルトはanalogic　他にもいくつかある
         @Query("format") format :String = "json",//結果を受け取る形式を指定する。デフォルトはjson 他にhtml,svg
         @Query("count") count : Int = 5,//取得するスキームカラーの数　デフォルトは5
-        ): ColorSchemeResponce
+        ): ColorSchemeResponse
 }
