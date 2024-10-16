fa804d6d282edcb378d8cd2de8a0979bfd5815a3
izumi <izumi@c538.local>
2024-10-16 21:01:06 +0900

    追加:戻り値のデータ型を ColorSchemeResponceとしました

--- app/src/main/java/com/example/findcolorcode/model/ColorSchemeResponse.kt
+++ app/src/main/java/com/example/findcolorcode/model/ColorSchemeResponse.kt
@@ -1,4 +1,14 @@
 package com.example.findcolorcode.model
 
-class ColorSchemeResponce {
-}
+//変数名とJSONのキー名は一致させている
+data class ColorSchemeResponse (
+
+    val mode:String,
+    val count:Int,
+    val colors:List<ColorCodesList>
+)
+
+data class ColorCodesList(
+    val value :String,
+    val clean: String
+)
