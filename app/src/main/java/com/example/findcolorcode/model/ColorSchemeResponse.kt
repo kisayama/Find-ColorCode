427719332b07ede605fb902ad0f12df079701daa
izumi <izumi@c538.local>
2024-10-19 00:06:59 +0900

    Moshi用のデータクラスを実装 -JSONのキー名と変数名を一致させる -@JsonClassを使用しKSPでアダプタを自動作成

--- app/src/main/java/com/example/findcolorcode/model/ColorSchemeResponse.kt
+++ app/src/main/java/com/example/findcolorcode/model/ColorSchemeResponse.kt
@@ -1,14 +1,23 @@
 package com.example.findcolorcode.model
 
-//変数名とJSONのキー名は一致させている
-data class ColorSchemeResponse (
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-    val mode:String,
-    val count:Int,
-    val colors:List<ColorCodesList>
+//変数名とJSONのキー名は一致させている
+
+//Moshiで使用するデータクラスには＠JsonClassをつける
+// (KSPでアダプタ(データ形式を変換する役割)を自動作成するためのアノテーション)
+//JsonとDataクラスの変数の名前が異なる時は以下の記入方法
+// @Json(name = "hidden_card") val hiddenCard: Card
+@JsonClass (generateAdapter = true)
+data class ColorSchemeResponse (
+    @Json val mode:String,
+    @Json val count:Int,
+    @Json val colors:List<ColorCodesList>
 )
 
+@JsonClass (generateAdapter = true)
 data class ColorCodesList(
-    val value :String,
-    val clean: String
+    @Json val value :String,
+    @Json val clean: String
 )
