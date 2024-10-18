1617f25fc04653a6bbcfe9237467138e4cfdb70b
izumi <izumi@c538.local>
2024-10-18 23:56:33 +0900

    追加: KSPとRoom、Moshiの依存関係

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
     implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0-rc01")
 
+
     implementation ("androidx.room:room-runtime:2.6.1") // Room
     ksp ("androidx.room:room-compiler:2.6.1") // Room用のKSP
     implementation ("com.squareup.moshi:moshi:1.12.0") // Moshi
