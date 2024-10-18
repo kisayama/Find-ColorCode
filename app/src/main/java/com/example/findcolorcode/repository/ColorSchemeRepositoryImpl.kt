2e39db7ef6473dc3d376343debe94770f30607d7
izumi <izumi@c538.local>
2024-10-19 00:17:33 +0900

    ColorSchemeRepositoryの実装クラスを作成 -Moshi、Retrofitのインスタンスを作成 -TheColorSpiServiceを使用してAPi通信を行うメソッドを実装 -今後apiServiceの実装部分とretrofitの実装部分でDIを使用する予定

--- app/src/main/java/com/example/findcolorcode/repository/ColorSchemeRepositoryImpl.kt
+++ app/src/main/java/com/example/findcolorcode/repository/ColorSchemeRepositoryImpl.kt
@@ -2,19 +2,34 @@ package com.example.findcolorcode.repository
 
 import com.example.findcolorcode.api.TheColorApiService
 import com.example.findcolorcode.model.ColorSchemeResponse
+import com.squareup.moshi.Moshi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 //APIと通信を行うクラス
 //プリマリコンストラクタで引数にtheColorApiServiceインスタンスを引き渡し依存性注入を行う
-//Diの使用頻度が高くなればHilt,Daggerを検討する
-class ColorSchemeRepositoryImpl(private val theColorApiService: TheColorApiService)
+class ColorSchemeRepositoryImpl(private val apiService: TheColorApiService)
     :ColorSchemeRepository {//Repository(InterFace)の実装
+
+    private val moshi : Moshi = Moshi.Builder().build()//Moshiのインスタンスを作成
+
+    //TODO　今後DIを使用する
+    //Retrofitのインスタンスを作成
+    //addConvertFactoryでJSONをObjectに変換するように設定する(Moshiを使用)
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://www.thecolorapi.com/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))//Moshi
+        .build()
+
+
     override suspend fun getColorScheme(
         colorCodeWithoutHash: String,
         mode: String,
         format: String,
         count: Int
-    ): ColorSchemeResponse {//ColorSchemeRepositoryで使用している戻り値の型を使用する
+    ): ColorSchemeResponse {//ColorSchemeRepositoryと同じ戻り値の型を使用する
         //APIサービスから結果を取得する
-        return theColorApiService.getColorScheme(colorCodeWithoutHash,mode,format,count)
+        return apiService.getColorScheme(colorCodeWithoutHash,mode,format,count)
     }
+
 }
