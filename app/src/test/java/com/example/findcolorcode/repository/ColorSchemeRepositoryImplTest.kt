8a3b76d2fa2b0e17f9d0db594948682d10a0bc26
izumi <izumi@c538.local>
2024-10-21 00:20:05 +0900

    追加: ColorSchemeRepositoryImplのテストケースを作成 - getColorSchemeメソッドのテストを行った - レスポンスの内容（mode, count, colors）が正しいかを確認 -カラーコードの出力を行った -レスポンス、カラーコードが正しい形式でレスポンスされたことを確認しました

--- app/src/test/java/com/example/findcolorcode/repository/ColorSchemeRepositoryImplTest.kt
+++ app/src/test/java/com/example/findcolorcode/repository/ColorSchemeRepositoryImplTest.kt
@@ -1,4 +1,30 @@
 package com.example.findcolorcode.repository
 
+import com.example.findcolorcode.api.TheColorApiService
+import com.example.findcolorcode.model.ColorCode
+import com.example.findcolorcode.model.ColorSchemeResponse
+import com.example.findcolorcode.model.HexValue
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.Mockito.*
+
 class ColorSchemeRepositoryImplTest {
+
+    private val repository = ColorSchemeRepositoryImpl()
+
+    @Test
+    fun testGetColorScheme() = runBlocking {
+        val colorCode = "ff0000"
+        val order = repository.getColorScheme(colorCode,"analogic", "json",5)
+        assertNotNull(order)
+        assertEquals("analogic",order.mode)
+        assertEquals(5,order.count)
+        assertTrue(order.colors.isNotEmpty())
+        order.colors.forEach{
+            colorCode ->
+            println("HexValue: ${colorCode.hex.value}, Clean: ${colorCode.hex.clean}")
+        }
+    }
+
 }
