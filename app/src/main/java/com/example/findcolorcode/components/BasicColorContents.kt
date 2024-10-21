35961aa80666a79a95b68276487e8f1028019961
izumi <izumi@c538.local>
2024-10-22 00:21:53 +0900

    追加：APIレスポンスデータを表示するコンテンツのコンポーネントを追加 BOX,BUtton

--- app/src/main/java/com/example/findcolorcode/components/BasicColorContents.kt
+++ app/src/main/java/com/example/findcolorcode/components/BasicColorContents.kt
@@ -1,6 +1,5 @@
 package com.example.findcolorcode.components
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,20 +15,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.findcolorcode.data.basicColorsList1
-import com.example.findcolorcode.data.basicColorsList2
-import com.example.findcolorcode.data.basicColorsList3
 import com.example.findcolorcode.viewmodel.ColorChoiceViewModel
 
 
 @Composable
 fun BasicColorContents(
- modifier: Modifier,
  viewModel: ColorChoiceViewModel,
  selectedSquare: Int,
  colorList1: List<String>,
  colorList2: List<String>,
- colorList3: List<String>)
+ colorList3: List<String>
+)
 {
  Column (modifier = Modifier
           .fillMaxWidth()
