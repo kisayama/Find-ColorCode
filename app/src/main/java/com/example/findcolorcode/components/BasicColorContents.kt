a51051a1d5a65732210c47b50bc44742152cadaa
izumi <izumi@c538.local>
2024-10-23 20:25:04 +0900

    整理：コメント整理

--- app/src/main/java/com/example/findcolorcode/components/BasicColorContents.kt
+++ app/src/main/java/com/example/findcolorcode/components/BasicColorContents.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -25,29 +26,34 @@ fun BasicColorContents(
  colorList1: List<String>,
  colorList2: List<String>,
  colorList3: List<String>
-)
-{
- Column (modifier = Modifier
-          .fillMaxWidth()
-          .padding(top = 66.dp, bottom = 10.dp),
-          verticalArrangement = Arrangement.spacedBy(10.dp)//オブジェクトごとに垂直方向に間隔を入れる
+) {
+ Column(
+  modifier = Modifier
+   .fillMaxWidth()
+   .padding(top = 72.dp) ,
+  verticalArrangement = Arrangement.Center,
+  horizontalAlignment = Alignment.CenterHorizontally
+ ) {
+  Column(
+   verticalArrangement = Arrangement.spacedBy(12.dp),//オブジェクトごとに垂直方向に間隔を入れる
   ) {
-  //BasicSquareが選択された時の処理
-  val onBasicSquareSelected :(String) -> Unit= {
-   //選択されたBasicSquareのカラーコードを取得する。
-   // updateColorメソッドを使用して現在選択しているsquareの背景の色を書き換える
-    selectedColorCode ->
-   //TextFieldの表示を変更
-   viewModel.updateColorCode(selectedSquare,selectedColorCode)
-   //squareの背景色を変更
-   viewModel.updateBackgroundColorCode(selectedSquare,selectedColorCode)
-   //RGB値を変更
-   viewModel.convertToRGB(selectedSquare)
+   //BasicSquareが選択された時の処理
+   val onBasicSquareSelected: (String) -> Unit = {
+    //選択されたBasicSquareのカラーコードを取得する。
+    // updateColorメソッドを使用して現在選択しているsquareの背景の色を書き換える
+     selectedColorCode ->
+    //TextFieldの表示を変更
+    viewModel.updateColorCode(selectedSquare, selectedColorCode)
+    //squareの背景色を変更
+    viewModel.updateBackgroundColorCode(selectedSquare, selectedColorCode)
+    //RGB値を変更
+    viewModel.convertToRGB(selectedSquare)
+   }
+   //縦に3つ並べる
+   BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList1, onBasicSquareSelected)
+   BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList2, onBasicSquareSelected)
+   BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList3, onBasicSquareSelected)
   }
-  //縦に3つ並べる
-  BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList1,onBasicSquareSelected)
-  BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList2,onBasicSquareSelected)
-  BasicColorRow(modifier = Modifier.weight(1f), colorList = colorList3,onBasicSquareSelected)
  }
 }
 
@@ -95,7 +101,7 @@ private fun BasicColorRow(
     .size(70.dp)
     .clickable { onBasicSquareSelected(colorCode) }
     .background(Color(android.graphics.Color.parseColor(colorCode)))
-    .border(1.dp, Color.LightGray)
+    .border(2.dp, Color.LightGray)
     .aspectRatio(1f)//1:1比率
   )
  }
