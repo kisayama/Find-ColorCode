00f00b33b64cc3f8332f72151e6b58c77fb145bb
izumi <izumi@c538.local>
2024-10-24 16:42:28 +0900

    整理:未使用のImportを削除

--- app/src/main/java/com/example/findcolorcode/repository/ColorSchemeRepository.kt
+++ app/src/main/java/com/example/findcolorcode/repository/ColorSchemeRepository.kt
@@ -1,7 +1,5 @@
 package com.example.findcolorcode.repository
 
-import com.example.findcolorcode.model.ColorSchemeResponse
-
 //RepositoryはAPIデータ取得を目的とするクラスに実装され、
 //クライアントからの指示でデータの取得を行う(窓口になる)
 // 実際にデータの取得を行うのはAPIサービス。
