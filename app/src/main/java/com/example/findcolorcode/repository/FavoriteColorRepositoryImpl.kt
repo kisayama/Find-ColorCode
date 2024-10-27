47efb0ffa8a2033d7c9d5efb334bb13bd673221f
izumi <izumi@c538.local>
2024-10-28 00:07:49 +0900

    追加:データベース操作用のレポジトリー実装クラスを追加しました

--- app/src/main/java/com/example/findcolorcode/repository/FavoriteColorRepositoryImpl.kt
+++ app/src/main/java/com/example/findcolorcode/repository/FavoriteColorRepositoryImpl.kt
@@ -2,9 +2,29 @@ package com.example.findcolorcode.repository
 
 import com.example.findcolorcode.data.ColorDao
 import com.example.findcolorcode.model.FavoriteColorDataClass
+import kotlinx.coroutines.flow.Flow
 
-class ColorRepositoryImpl(private val colorDao: ColorDao) {
+//repositoryの実装クラス
+//ColorDaoの5つのメソッドをオーバーライドする
+class FavoriteColorRepositoryImpl(private val colorDao: ColorDao):FavoriteColorRepository {
     override suspend fun insertColor(color: FavoriteColorDataClass){
-        ColorDao.
+        colorDao.insertColor(color)
+    }
+
+    override suspend fun getAllColors(): List<FavoriteColorDataClass> {
+        return colorDao.getAllColors()
+    }
+
+    override fun getColorById(colorId: String): Flow<FavoriteColorDataClass> {
+        return colorDao.getColorById(colorId)
+    }
+
+
+    override suspend fun updateColor(color: FavoriteColorDataClass) {
+        colorDao.updateColor(color)
+    }
+
+    override suspend fun deleteColor(color: FavoriteColorDataClass) {
+        colorDao.deleteColor(color)
     }
 }
