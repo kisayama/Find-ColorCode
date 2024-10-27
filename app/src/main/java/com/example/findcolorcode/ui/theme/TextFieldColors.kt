02784f80a08d055452cbf7dd95c7f94b4ae812af
izumi <izumi@c538.local>
2024-10-27 13:24:10 +0900

    追加：テキストフィールドの共通のテーマを作成しました -ColorChoiceScreen,ColorSaveDialogのテキストフィールドに適用しました

---
+++ app/src/main/java/com/example/findcolorcode/ui/theme/TextFieldColors.kt
@@ -0,0 +1,21 @@
+package com.example.findcolorcode.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.example.app.ui.theme.AppColors
+
+
+@Composable
+fun customTextFieldColors(): TextFieldColors {
+    return TextFieldDefaults.colors(
+        focusedContainerColor = AppColors.White,//フォーカス時の色
+        unfocusedContainerColor = AppColors.White,//非フォーカス時の色
+        focusedIndicatorColor = AppColors.Black,//フォーカス時のインジケーター
+        focusedLabelColor = AppColors.Gray,// フォーカス時のラベル
+        unfocusedLabelColor = AppColors.Gray //非フォーカス時のラベル
+    )
+}
+
