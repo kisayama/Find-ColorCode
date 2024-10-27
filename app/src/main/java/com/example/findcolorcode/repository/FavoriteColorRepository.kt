26a8058cdea4655c7ddf349af191fdae4c1de3f8
izumi <izumi@c538.local>
2024-10-28 00:07:09 +0900

    追加:データベース操作用のレポジトリーを追加しました

--- app/src/main/java/com/example/findcolorcode/repository/FavoriteColorRepository.kt
+++ app/src/main/java/com/example/findcolorcode/repository/FavoriteColorRepository.kt
@@ -1,4 +1,14 @@
 package com.example.findcolorcode.repository
 
+import com.example.findcolorcode.model.FavoriteColorDataClass
+import kotlinx.coroutines.flow.Flow
+
+
 interface FavoriteColorRepository {
+    suspend fun insertColor(color:FavoriteColorDataClass)
+    suspend fun getAllColors():List<FavoriteColorDataClass>
+    fun getColorById(colorId:String):Flow<FavoriteColorDataClass>
+    suspend fun updateColor(color:FavoriteColorDataClass)
+    suspend fun deleteColor(color:FavoriteColorDataClass)
+
 }
