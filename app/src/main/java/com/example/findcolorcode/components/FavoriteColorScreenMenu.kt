69986a0ed7bff944bff1634d97d1f97653c8d646
izumi <izumi@c538.local>
2024-11-07 16:57:50 +0900

    追加:DropdownMenuを追加しました -各メニューを選択した時の操作を定義しました

--- app/src/main/java/com/example/findcolorcode/components/FavoriteColorScreenMenu.kt
+++ app/src/main/java/com/example/findcolorcode/components/FavoriteColorScreenMenu.kt
@@ -1,14 +1,42 @@
 package com.example.findcolorcode.components
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.LocalContext
+import com.example.findcolorcode.data.favoriteColorScreenMenu
+import com.example.findcolorcode.model.FavoriteColorDataClass
+import com.example.findcolorcode.viewmodel.FavoriteScreenViewModel
 
 @Composable
- fun FavoriteColorListMenu(){
+ fun FavoriteColorListMenu(colorItem:FavoriteColorDataClass,viewModel: FavoriteScreenViewModel){
 
+     //トーストとクリップボードへの保存に使用するContext
+    val context = LocalContext.current
      DropdownMenu(
-         expanded = ,
-         onDismissRequest = { /*TODO*/ }) {
-
+         expanded = viewModel.menuExpand.value?:false,
+         onDismissRequest = { viewModel.updateMenuExpand(false) }
+     ) {
+         favoriteColorScreenMenu.forEachIndexed{index, menu ->
+             DropdownMenuItem(
+                 text = {Text(text = menu)},
+                 onClick = {
+                     viewModel.updateMenuExpand(false)
+                     when(index){
+                         //クリップボードにカラーコードをコピー 
+                         0 -> {
+                             viewModel.copyToClipBoard(context, colorItem.colorCode)
+                         }
+                         //色情報を変更するダイアログを表示
+                         1 -> viewModel.updateOpenDialog(true)//TODO ダイアログの再利用
+                         //色をデータベースから削除する
+                         2 -> viewModel.deleteColors(colorItem.id)
+                         else -> {}//例外では何も行わない
+                     }
+                 }
+             )
+             }
+         }
      }
- }
