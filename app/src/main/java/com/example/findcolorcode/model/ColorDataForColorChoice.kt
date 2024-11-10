6796b3bd09f2973cb2b0b0cc9ebf4cd9d402f395
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-10 16:42:04 +0900

    整理：可読性を高めるためにインデントの整理を行いました

--- app/src/main/java/com/example/findcolorcode/model/ColorDataForColorChoice.kt
+++ app/src/main/java/com/example/findcolorcode/model/ColorDataForColorChoice.kt
@@ -1,3 +1,9 @@
 package com.example.findcolorcode.model
 
-    data class ColorDataForColorChoice(val colorCode: String, val backgroundColorCode:String, val red: Int, val green: Int, val blue: Int)
+    data class ColorDataForColorChoice(
+        val colorCode: String,
+        val backgroundColorCode:String,
+        val red: Int,
+        val green: Int,
+        val blue: Int
+    )
