6ac7b8d2652d38c530391bc4eaad57a81daa4ae5
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-13 00:15:58 +0900

    追加:favoriteColorScreenSortMenuLstを追加 -昇順降順を指定するためのメニューに表示するテキストを定義したリスト

--- app/src/main/java/com/example/findcolorcode/data/MenuList.kt
+++ app/src/main/java/com/example/findcolorcode/data/MenuList.kt
@@ -3,3 +3,7 @@ package com.example.findcolorcode.data
 val favoriteColorScreenMenuList = listOf(
     "カラーコードをコピー","変更","削除"
 )
+
+val favoriteColorScreenSortMenuList = listOf(
+    "日付:新しい順","日付：古い順"
+)
