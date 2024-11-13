2e992e608fada2710b75b8b6cdf9889eadd2c13d
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-13 21:27:23 +0900

    整理：コメントを追加しました 整理：インデントを整えました

--- app/src/main/java/com/example/findcolorcode/components/BottomBar.kt
+++ app/src/main/java/com/example/findcolorcode/components/BottomBar.kt
@@ -10,18 +10,21 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
-
 @Composable
-//selectedItemは現在選択しているアイテムのインデックス
-fun BottomBar(navController: NavController, selectedItem: Int, onItemSelected: (Int) -> Unit) {
+//selectedItemは選択中のタブのインデックスを示す
+fun BottomBar(
+    navController: NavController,
+    selectedItem: Int,
+    onItemSelected: (Int) -> Unit) {
     NavigationBar {
-        BottomBarTab.values().forEachIndexed { index, item ->
+        //表示項目をenumClassで列挙する
+        BottomBarTab.entries.forEachIndexed { index, item ->
             NavigationBarItem(
                 //タブが選択されているかを判定
                 selected = selectedItem == index,
                 //クリック時の動作を定義
                 onClick = {
-                    onItemSelected(index)
+                    onItemSelected(index)//indexを呼び出し元コンポーネントに渡す
                     navController.navigate(item.route)
                 },
                 //Iconとlabelを指定
