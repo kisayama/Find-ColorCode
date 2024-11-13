cc6deb8a55548ee499ec71d38aec1fb1dcd224c3
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-13 17:56:16 +0900

    修正: ベースURLにエンドポイントを設定していたのを修正し、エンドポイントを正しく設定しました 整理：インデントを整えました

--- app/src/main/java/com/example/findcolorcode/api/TheColorApiService.kt
+++ app/src/main/java/com/example/findcolorcode/api/TheColorApiService.kt
@@ -5,11 +5,16 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TheColorApiService {
-    @GET("https://www.thecolorapi.com/scheme?")
+    //エンドポイントを指定
+    @GET("scheme")
     suspend fun getColorScheme(
-        @Query("hex") colorCodeWithoutHash :String,// #を除いたcolorCode　例: 0047AB
-        @Query("mode") mode :String = "analogic",//スキームを生成するためのモード デフォルトはanalogic　他にもいくつかある
-        @Query("format") format :String = "json",//結果を受け取る形式を指定する。デフォルトはjson 他にhtml,svg
-        @Query("count") count : Int = 5,//取得するスキームカラーの数　デフォルトは5
-        ): ColorSchemeResponse
+        // #を除いたcolorCode　例: 0047AB
+        @Query("hex") colorCodeWithoutHash: String,
+        //スキームを生成するためのモード デフォルトはanalogic　他にもいくつかある
+        @Query("mode") mode: String = "analogic",
+        //結果を受け取る形式を指定する。デフォルトはjson 他にhtml,svg
+        @Query("format") format: String = "json",
+        //取得するスキームカラーの数　デフォルトは5
+        @Query("count") count: Int = 5,
+    ): ColorSchemeResponse
 }
