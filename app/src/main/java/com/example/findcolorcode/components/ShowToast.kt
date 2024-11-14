e1d55eb9ac9318f9f4d8d0cffcd1c41df00abd15
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-14 18:38:46 +0900

    整理：コメントの追加、修正

--- app/src/main/java/com/example/findcolorcode/components/ShowToast.kt
+++ app/src/main/java/com/example/findcolorcode/components/ShowToast.kt
@@ -4,19 +4,18 @@ import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.ViewModel
-import com.example.findcolorcode.viewmodel.ColorChoiceViewModel
-import com.example.findcolorcode.viewmodel.MainViewModel
 
 //Toastメッセージが変更されたことを検知してToastを表示する
 //複数のViewModelで使用予定
 @Composable
-fun ShowToast(toastMessage: String,resetMessage:() -> Unit) {
+fun ShowToast(toastMessage: String, resetMessage: () -> Unit) {
     val context = LocalContext.current
     //LaunchedEffectは指定したキーが変更された時に{}内を実行する
     if (toastMessage.isNotEmpty()) {
         LaunchedEffect(toastMessage) {
             Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
+            //トーストメッセージを空にするメソッド
+            //これにより同じメッセージを連続で送ることができる
             resetMessage()
         }
     }
