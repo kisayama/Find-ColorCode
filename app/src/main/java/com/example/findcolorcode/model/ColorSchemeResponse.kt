89b34f471b3fd020ba3dbed9f934523853a63f04
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-14 22:13:56 +0900

    整理：コメントを追加

--- app/src/main/java/com/example/findcolorcode/model/ColorSchemeResponse.kt
+++ app/src/main/java/com/example/findcolorcode/model/ColorSchemeResponse.kt
@@ -3,25 +3,24 @@ package com.example.findcolorcode.model
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-//変数名とJSONのキー名は一致させている
-
 //Moshiで使用するデータクラスには＠JsonClassをつける
 // (KSPでアダプタ(データ形式を変換する役割)を自動作成するためのアノテーション)
+//データクラスの各プロパティ名とJSONのキー名は一致している必要がある
 //JsonとDataクラスの変数の名前が異なる時は以下の記入方法
 // @Json(name = "hidden_card") val hiddenCard: Card
 @JsonClass (generateAdapter = true)
 data class ColorSchemeResponse (
-    @Json val mode:String,
-    @Json val count:Int,
-    @Json val colors:List<ColorCode>
+    @Json val mode:String,//GET時に指定した色のモード
+    @Json val count:Int,//指定したカラーコードの数
+    @Json val colors:List<ColorCode>//受け取った色リスト
 )
 @JsonClass (generateAdapter = true)
 data class ColorCode(
-    @Json val hex :HexValue
+    @Json val hex :HexValue//#付きHex、#なしHexの2種類を保持する
 )
 
 @JsonClass (generateAdapter = true)
 data class HexValue(
-    @Json val value :String,
-    @Json val clean: String
+    @Json val value :String,//#がついたHex
+    @Json val clean: String//#がついていないHex
 )
