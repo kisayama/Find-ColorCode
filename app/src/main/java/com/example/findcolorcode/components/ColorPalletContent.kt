0211bb10847c573c2bf949d93092c144e13a178d
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-14 22:01:50 +0900

    整理：不要なImportを削除

--- app/src/main/java/com/example/findcolorcode/components/ColorPalletContent.kt
+++ app/src/main/java/com/example/findcolorcode/components/ColorPalletContent.kt
@@ -19,7 +19,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.findcolorcode.model.ColorDataForColorChoice
 import com.example.findcolorcode.viewmodel.ColorChoiceViewModel
