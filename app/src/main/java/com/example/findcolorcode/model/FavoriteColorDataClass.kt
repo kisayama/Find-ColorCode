4fd0c361ccc2516249b7cd52fc94a34fa823afbc
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-18 00:04:44 +0900

    整理：コメントを追加

--- app/src/main/java/com/example/findcolorcode/model/FavoriteColorDataClass.kt
+++ app/src/main/java/com/example/findcolorcode/model/FavoriteColorDataClass.kt
@@ -5,13 +5,17 @@ import androidx.room.Entity
 import com.squareup.moshi.JsonClass
 import java.util.UUID
 
+//データベースに保存する形式のデータクラス
 @JsonClass(generateAdapter = true)
 @Entity(tableName = "FavoriteColorDataClass")//エンティティとして指定
 data class FavoriteColorDataClass(
     @PrimaryKey val id:String = UUID.randomUUID().toString(),
     //backgroundColorCode
     val colorCode:String,
+    //ユーザーがダイアログ上に入力した名前
     var colorName:String,
+    //ユーザーがダイアログ上に入力したメモ
     var colorMemo: String,
+    //作成・変更した日時どちらか新しい方
     var editDateTime:Long
 )
