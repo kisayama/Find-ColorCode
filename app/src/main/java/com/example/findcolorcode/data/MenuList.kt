0dd73df6c81b28eb9ef472f07c75c78ecc975dbe
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-20 23:29:15 +0900

    追加：メニューを追加 ColorChoiceScreenに遷移し、カラーコードを引き渡す選択肢を追加しました

--- app/src/main/java/com/example/findcolorcode/data/MenuList.kt
+++ app/src/main/java/com/example/findcolorcode/data/MenuList.kt
@@ -3,7 +3,7 @@ package com.example.findcolorcode.data
 
 //お気に入りの色に対して実行できる操作を定義したメニューリスト
 val favoriteColorActionsMenuList = listOf(
-    "カラーコードをコピー","変更","削除"
+    "左パレットに移動","右パレットに移動","カラーコードをコピー","変更","削除"
 )
 //お気に入りの色を並び替える条件を定義したメニューリスト
 val favoriteColorScreenSortMenuList = listOf(
