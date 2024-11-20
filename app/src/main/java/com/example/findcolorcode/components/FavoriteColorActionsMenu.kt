e582b3f14aea0d3507237070e09d0d858ace4d00
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-20 23:29:01 +0900

    追加：メニューを追加 ColorChoiceScreenに遷移し、カラーコードを引き渡す選択肢を追加しました

--- app/src/main/java/com/example/findcolorcode/components/FavoriteColorActionsMenu.kt
+++ app/src/main/java/com/example/findcolorcode/components/FavoriteColorActionsMenu.kt
@@ -1,19 +1,25 @@
 package com.example.findcolorcode.components
 
+import android.annotation.SuppressLint
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavDestination.Companion.createRoute
+import androidx.navigation.NavHostController
 import com.example.findcolorcode.data.favoriteColorActionsMenuList
 import com.example.findcolorcode.model.FavoriteColorDataClass
 import com.example.findcolorcode.viewmodel.FavoriteScreenViewModel
+import java.net.URLEncoder
 
 
 //FavoriteListScreen内のリストのmoreボタンを押すと表示されるメニュー
 //選択しているお気に入りの色に関連する操作を行う
 @Composable
  fun FavoriteColorActionsMenu(
+    //ColorChoiceScreenに遷移する
+    navController: NavHostController,
     //現在選択している色
     colorItem:FavoriteColorDataClass,
     viewModel: FavoriteScreenViewModel,
@@ -34,12 +40,25 @@ import com.example.findcolorcode.viewmodel.FavoriteScreenViewModel
                  onClick = {
                      closeMenuExpand()//メニューを閉じる
                      when(index){
-                         //クリップボードにカラーコードをコピー 
-                         0 -> viewModel.copyToClipBoard(context, colorItem.colorCode)
+                         //ColorChoiceScreenに遷移する
+                         //同時に左フラグ、色コードを引き渡す
+                         0 -> moveToColorChoice(
+                             navController,
+                             "left",
+                             colorItem.colorCode
+                         )
+                         //ColorChoiceScreenに遷移する
+                         //同時に右フラグ、色コードを引き渡す
+                         1 -> moveToColorChoice(
+                             navController,
+                             "right",
+                             colorItem.colorCode)
+                         //クリップボードにカラーコードをコピー
+                         2 -> viewModel.copyToClipBoard(context, colorItem.colorCode)
                          //色情報を変更するダイアログを表示
-                         1 -> viewModel.updateDialogOpen(true)
+                         3 -> viewModel.updateDialogOpen(true)
                          //色をデータベースから削除する
-                         2 -> viewModel.deleteColors(colorItem.id)
+                         4 -> viewModel.deleteColors(colorItem.id)
                          else -> {}//その他は何もしない
                      }
                  }
@@ -47,3 +66,13 @@ import com.example.findcolorcode.viewmodel.FavoriteScreenViewModel
              }
          }
      }
+@SuppressLint("RestrictedApi")
+fun moveToColorChoice(navController:NavHostController, direction:String, colorCode:String){
+    val encodedColorCode = URLEncoder.encode(colorCode,"UTF-8")
+     navController.navigate("colorChoice?direction=${direction}&colorCode=${encodedColorCode}")
+    val route = createRoute("colorChoice?direction=${direction}&colorCode=${encodedColorCode}")
+}
+
+
+
+
