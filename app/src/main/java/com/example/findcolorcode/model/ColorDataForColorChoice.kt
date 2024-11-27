de6f3ecd5672ca640dfd925060b70879c36ff6f3
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-28 00:13:32 +0900

    整理：コメントの追加

--- app/src/main/java/com/example/findcolorcode/model/ColorDataForColorChoice.kt
+++ app/src/main/java/com/example/findcolorcode/model/ColorDataForColorChoice.kt
@@ -2,7 +2,7 @@ package com.example.findcolorcode.model
 
 //ColorChoiceScreenで色の作成中に使用する一時的なデータクラス
     data class ColorDataForColorChoice(
-        //TextFieldに表示するカラーコード
+        //ユーザーが編集しTextFieldに表示するカラーコード
         val colorCode: String,
         //プレビュー用Boxの背景色に指定するカラーコード
         val backgroundColorCode:String,
