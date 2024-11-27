843bbf73791032f558505ded403a278c6e5c94af
kisayama <163150888+kisayama@users.noreply.github.com>
2024-11-28 00:00:58 +0900

    整理：不要なPreviewコンポーネントを削除しました。 不要なインポート分を削除しました

--- app/src/main/java/com/example/findcolorcode/components/AdjustValueBar.kt
+++ app/src/main/java/com/example/findcolorcode/components/AdjustValueBar.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.findcolorcode.R
 
@@ -64,10 +63,4 @@ fun AdjustValueBar(
             )
         }
     }
-}
-
-@Preview
-@Composable
-fun PreviewAdjustValueBar() {
-    AdjustValueBar(adjustValue = {})
 }
