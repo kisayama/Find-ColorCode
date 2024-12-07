7a16e22feac2a0c6e5a62083b1e7544e808dd9fa
kisayama <163150888+kisayama@users.noreply.github.com>
2024-12-08 01:11:22 +0900

    修正：focusedContainerColor、unfocusedContainerColorのBackgroundColorをTransparentに変更しました Viewの邪魔をしないために

--- app/src/main/java/com/example/findcolorcode/ui/theme/TextFieldColors.kt
+++ app/src/main/java/com/example/findcolorcode/ui/theme/TextFieldColors.kt
@@ -3,14 +3,15 @@ package com.example.findcolorcode.ui.theme
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.example.app.ui.theme.AppColors
 
 
 @Composable
 fun customTextFieldColors(): TextFieldColors {
     return TextFieldDefaults.colors(
-        focusedContainerColor = AppColors.White,//フォーカス時の色
-        unfocusedContainerColor = AppColors.White,//非フォーカス時の色
+        focusedContainerColor = Color.Transparent,//フォーカス時の色
+        unfocusedContainerColor = Color.Transparent,//非フォーカス時の色
         focusedIndicatorColor = AppColors.Black,//フォーカス時のインジケーター
         focusedLabelColor = AppColors.Gray,// フォーカス時のラベル
         unfocusedLabelColor = AppColors.Gray //非フォーカス時のラベル
