af06d71714ea9b2cce33b9ca4cc88510d807f1bc
kisayama <163150888+kisayama@users.noreply.github.com>
2024-12-08 01:12:00 +0900

    整理：不要なインポートを削除しました

--- app/src/main/java/com/example/findcolorcode/view/FavoriteListScreen.kt
+++ app/src/main/java/com/example/findcolorcode/view/FavoriteListScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
