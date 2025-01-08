781f7a973f5e0c140de8fac059ebcb148dee42ae
kisayama <163150888+kisayama@users.noreply.github.com>
2025-01-08 22:13:12 +0900

    修正：versionName,versionCodeを変更しました

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,9 +16,9 @@ android {
     defaultConfig {
         applicationId = "com.kisayama.findcolorcode"
         minSdk = 23
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
