c02bbcfabc3526b772b74b3281a7523f46bb9bb0
kisayama <163150888+kisayama@users.noreply.github.com>
2025-01-11 22:24:11 +0900

    追加：APIに送るmodeを変更するためのボタンを追加 -現在選んでいるモードがボタンのテキストとして表示される -プッシュ時に選択肢が表示される

---
+++ app/src/main/java/com/example/findcolorcode/components/ChangePalletModeButton.kt
@@ -0,0 +1,83 @@
+package com.example.findcolorcode.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.findcolorcode.data.colorSchemeModeList
+import com.example.findcolorcode.ui.theme.getDynamicTypography
+import com.kisayama.findcolorcode.R
+
+@Composable
+fun ChangePalletModeButton(
+    currentMode:String,
+    updateValue: (String) -> Unit
+){
+    var mode by remember { mutableStateOf(currentMode) }
+    var modeDescription by remember(mode) {
+        mutableStateOf(when(mode){
+            "analogic" -> "調和的、穏やか"
+            "complement" -> "コントラスト"
+            "analogic-complement" -> "バランス"
+            "triad" -> "鮮やか"
+            "quad" -> "とても鮮やか"
+            "monochrome" -> "モノクロ"
+            "monochrome-light" -> "モノクロライト"
+            "monochrome-dark" -> "モノクロダーク"
+            else -> "未設定"
+         }
+        )
+    }
+
+    var isMenuOpen by remember { mutableStateOf(false) }
+    val fontSize = getDynamicTypography().bodyLarge.fontSize
+
+    OutlinedButton(
+        onClick = {isMenuOpen = true},
+        modifier = Modifier
+            .width(180.dp)
+    ){
+        Row(
+            horizontalArrangement = Arrangement.Start, // アイコンを一番左に固定　動かないように
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_changed),
+                contentDescription = "モード変更",
+                tint = Color.Black
+            )
+        }
+            Text(
+                modifier = Modifier.padding(start = 3.dp), text = modeDescription,
+                fontSize = fontSize
+            )
+
+        DropdownMenu(
+            expanded = isMenuOpen,
+            onDismissRequest = { isMenuOpen = false }) {
+            colorSchemeModeList.forEach { menu ->
+                DropdownMenuItem(
+                    text = { Text(text = menu.first)},
+                    onClick = {
+                        updateValue(menu.second)
+                        modeDescription = menu.first
+                        isMenuOpen = false
+                    }
+                )
+            }
+        }
+    }
+}
