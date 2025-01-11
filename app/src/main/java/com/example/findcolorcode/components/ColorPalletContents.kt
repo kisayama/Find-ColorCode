f1442eaf2e1b926be003a9b85cc40b05b9b66b14
kisayama <163150888+kisayama@users.noreply.github.com>
2025-01-11 23:23:55 +0900

    修正：レイアウトの位置関係の調整のためのインポートを調整

--- app/src/main/java/com/example/findcolorcode/components/ColorPalletContents.kt
+++ app/src/main/java/com/example/findcolorcode/components/ColorPalletContents.kt
@@ -9,19 +9,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.findcolorcode.model.ColorDataForColorChoice
+import com.example.findcolorcode.ui.theme.getDynamicTypography
 import com.example.findcolorcode.viewmodel.ColorChoiceViewModel
 
 // 選択した色のカラーコードを基に、APIからカラーパレットを取得するView
